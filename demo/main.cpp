1c665ccd945e8b26d2afa1b50a4f2b68747ea650
Carter <137724610+cartercpp@users.noreply.github.com>
2026-09-05 19:50:33 -0500

    Update main.cpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -116,7 +116,7 @@ int main()
                             break;
                     }
 
-                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
             }
         }};
